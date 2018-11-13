8f102afb22fffb70374f065cf65b74e0d9b73b07
Andy (me) <aguinto@andys-macbook-pro.local>
2018-11-12 19:52:51 -0500

    Initial Commit

---
+++ InstagramDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramDemo
+//
+//  Created by Andy Guinto on 11/12/18.
+//  Copyright © 2018 Andy Guinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
